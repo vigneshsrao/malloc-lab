9127b1c5b3b2eb957a30fb5f01ac3453d7f6f9e9
vigneshsrao <vigneshsrao5@gmail.com>
2018-01-16 16:02:10 +0530

    basic top chunk

--- mm.c
+++ mm.c
@@ -1,6 +1,6 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -26,9 +26,9 @@ team_t team = {
     /* Team name */
     "ateam",
     /* First member's full name */
-    "Harry Bovik",
+    "Vignesh S Rao",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "vigneshsrao5@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
@@ -41,31 +41,71 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/* 
+void* heap_start;  /* Pointer to starting of the heap */
+void* top;  /* Pointer to the top chunk */
+
+/* Macros for the chunk fields */
+#define prev_size(p) ((size_t*)p)
+#define chunk_size(p) ((size_t *)(p + ALIGNMENT))
+#define fd(p) ((size_t*)p + (size_t)2*ALIGNMENT)
+#define bk(p) ((size_t*)p + (size_t) 3*ALIGNMENT)
+
+void mm_checkheap()
+{
+  printf("\np = %p\n",heap_start);
+  printf("prev size 1st= %x\n",*prev_size(heap_start));
+  printf("size 1st= %x\n",*chunk_size(heap_start));
+
+  printf("\ntop = %p\n",top);
+  printf("prev size top= %x\n",*prev_size(top));
+  printf("size top= %x\n",*chunk_size(top));
+}
+
+/*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    top=mem_sbrk(ALIGN(0x31000));
+    heap_start=top;
+    //mm_checkheap();
+    if(top==(void*)-1){
+      printf("\n[ERROR] mem_sbrk failed\n");
+      exit(1);
+    }
+    //memset(top,0,0x31000);
+    *chunk_size(top) = (size_t)ALIGN(0x31000);
+    //mm_checkheap();
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size)
 {
     int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    // void *p = mem_sbrk(newsize);
+    if((*(int*)chunk_size(top))<newsize){
+      printf("[ERROR] Out of memory !");
+      exit(1);
     }
+    void* p = top;
+    size_t prev=*chunk_size(top);
+    top += newsize;
+    *chunk_size(top)=prev-newsize;
+    *chunk_size(p)=newsize;
+    //mm_checkheap();
+    // if (p == (void *)-1)
+	  //     return NULL;
+    // else {
+    //     *(size_t *)p = size;
+    //     return (void *)((char *)p + 2*SIZE_T_SIZE);
+    // }
+    return (void *)((char *)p + 2*SIZE_T_SIZE);
 }
 
 /*
@@ -83,7 +123,7 @@ void *mm_realloc(void *ptr, size_t size)
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
-    
+
     newptr = mm_malloc(size);
     if (newptr == NULL)
       return NULL;
@@ -94,17 +134,3 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
